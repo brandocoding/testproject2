d33c96174a983d21300a2a8243a757f5eba13092
brandocoding <41130732+brandocoding@users.noreply.github.com>
2021-04-13 18:47:38 -0500

    Add files via upload

---
+++ ObjectCollision-Test_1/settings.py
@@ -0,0 +1,15 @@
+WIDTH = 480
+HEIGHT = 720
+FPS = 60
+
+TITLE = "Test"
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+SPACING_3 = 120
+LOG_WIDTH = 90
+BOX_WIDTH = 30
+neg_logspeed = -1
---
+++ ObjectCollision-Test_1/sprites.py
@@ -0,0 +1,37 @@
+import pygame
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, game):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 30))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 96
+        self.speedx = 0
+        self.speedy = 0
+        self.onLog = False
+
+    def update(self):
+        if self.onLog:
+            self.speedx = neg_logspeed
+            self.speedy = 0
+            print(self.onLog)
+
+class LogRows(pygame.sprite.Sprite):
+    def __init__(self, speed, spacing, start_x, bottom, width):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((width, 30))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = (start_x * (spacing + BOX_WIDTH))
+        self.rect.bottom = bottom
+        self.speedx = speed
+
+    def update(self):
+        self.rect.x += self.speedx
+        if self.rect.x < -30:
+            self.rect.x = 480
+        if self.rect.x > 480:
+            self.rect.x = -30
